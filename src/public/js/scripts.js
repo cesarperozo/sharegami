6c5f682f1fa5e26c76c58ae5c63f43c297b5de8b
Cesar perozo <cesarmataperozo@gmail.com>
2019-06-12 17:22:54 -0300

    first commit

---
+++ src/public/js/scripts.js
@@ -0,0 +1,36 @@
+$("#post-comment").hide();
+$("#btn-toggle-comment").click(e => {
+  e.preventDefault();
+  $("#post-comment").slideToggle();
+});
+
+$("#btn-like").click(function(e) {
+  e.preventDefault();
+  let imgId = $(this).data("id");
+
+  $.post("/images/" + imgId + "/like").done(data => {
+    console.log(data);
+    $(".likes-count").text(data.likes);
+  });
+});
+
+$("#btn-delete").click(function(e) {
+  e.preventDefault();
+  let $this = $(this);
+  const response = confirm("Are you sure you to delete this image?");
+  if (response) {
+    let imgId = $(this).data("id");
+    $.ajax({
+      url: "/images/" + imgId,
+      type: "DELETE"
+    }).done(function(result) {
+      $this.removeClass("btn-danger").addClass("btn-success");
+      $this
+        .find("i")
+        .removeClass("fa-times")
+        .addClass("fa-check");
+      $this.append("<span>Deleted!</sapn>");
+      window.location.href = "/";
+    });
+  }
+});
---
+++ src/server/config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const exphbs = require('express-handlebars');
+
+const morgan = require('morgan');
+const multer = require('multer');
+const express = require('express');
+const erroHandler = require('errorhandler');
+
+const routes = require('../routes/index');
+
+
+
+module.exports = app => {
+
+    //settings
+    app.set('port', process.env.PORT || 5000);
+    app.set('views', path.join(__dirname, '../views'));
+    app.engine('.hbs', exphbs({
+        defaultLayout: 'main',
+        partialsDir: path.join(app.get('views'), 'partials'),
+        layoutsDir: path.join(app.get('views'), 'layouts'),
+        extname: '.hbs',
+        helpers: require('./helpers')
+    }));
+
+    app.set('view engine', '.hbs');
+
+    //middlewares
+    app.use(morgan('dev'));
+    app.use(multer({dest: path.join(__dirname, '../public/upload/temp')}).single('image'));
+    app.use(express.urlencoded({extended: false}));
+    app.use(express.json());
+
+    //routes
+    routes(app);
+
+    //static files
+    app.use('/public', express.static(path.join(__dirname, '../public')));
+
+    // errorgandlers
+    if ('development' === app.get('env')) {
+        app.use(erroHandler);
+    }
+
+    return app;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
